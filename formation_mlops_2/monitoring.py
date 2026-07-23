e7af918d511f10ddd23c30a6af102406816f3cbd
Arivima <arielle.villamassone@gmail.com>
2026-07-23 11:28:24 +0000

    Graphana

--- formation_mlops_2/monitoring.py
+++ formation_mlops_2/monitoring.py
@@ -3,9 +3,7 @@ import pandas as pd
 
 def monitor(latest_predictions: pd.DataFrame) -> pd.DataFrame:
     # Start filling function
-    monitoring_df = pd.DataFrame({
-        "predictions_time": [None],
-        "predictions": [None]
-    })
+    monitoring_df = latest_predictions.groupby('predictions_time')['predictions'].mean().reset_index()
+ 
     # End filling function
     return monitoring_df
