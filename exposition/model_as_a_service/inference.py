c55e93144c0f7d3b6673a653a7dff6364cee5408
Arivima <arielle.villamassone@gmail.com>
2026-07-23 13:00:48 +0000

    Pydantic

--- exposition/model_as_a_service/inference.py
+++ exposition/model_as_a_service/inference.py
@@ -3,16 +3,19 @@ from config import MODEL_PATH
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel, Field
+
 from formation_mlops_2.feature_engineering import prepare_features
 from formation_mlops_2.train_and_predict import predict
 
 app = FastAPI()
 
-
 class PredictionQueryParams(BaseModel):
-    Ws1_avg: int = Field(
-        gt=...,
-        description="..."
+    Ws1_avg: float = Field(
+        gt=0, lt=1.94, description="Validate..."
     )
 
 
@@ -22,8 +25,8 @@ def health():
 
 
 @app.get("/predict")
-def predict_endpoint(Ws1_avg: str):  # noqa
-    received_wind_speed_avg = Ws1_avg
+def predict_endpoint(params: Annotated[PredictionQueryParams, Depends()]):  # noqa
+    received_wind_speed_avg = params['Ws1_avg']
     received_data = {
         "Wind_turbine_name": "R80721",
         "Date_time": "2017-02-08T08:00:00+01:00",
